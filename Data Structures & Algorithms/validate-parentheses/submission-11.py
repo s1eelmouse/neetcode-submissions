a36f318deddd215bc9d7e3ed52068bdadc8ec84b
s1eelmouse <95374904+s1eelmouse@users.noreply.github.com>
2026-05-04 00:53:36 +0300

    Add: validate-parentheses - submission-11

---
+++ Data Structures & Algorithms/validate-parentheses/submission-11.py
@@ -0,0 +1,26 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        leftToRight = {
+            "(" : ")",
+            "[" : "]",
+            "{" : "}",
+        }
+        if len(s) % 2 == 0:
+            while s != "":
+                if s[0] in leftToRight.keys():
+                    if leftToRight[s[0]] != s[-1]:
+                        if leftToRight[s[0]] != s[1]:
+                            return False
+                            break
+                        else:
+                            s = s[2::]                
+                    else:
+                        s = s[1:-1]
+                else:
+                    return False
+                    break
+            return True
+        return False
+
+
+        
